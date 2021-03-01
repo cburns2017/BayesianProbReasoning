dac3f258a5796f09408a738607ac5e2fccfe6ed4
car5501 <cepeda.carlos2310@gmail.com>
2021-03-01 17:17:07 -0500

    Added input functionality for Source nodes and started test input file and input template
    Im making these changes so you can see how i am approaching this.

--- main.cpp
+++ main.cpp
@@ -9,26 +9,40 @@ using namespace std;
 
 int main()
 {
-   cout << "Hello World!" << endl;
-   vector<char> vec; 
-   vec.push_back('B');
-   vec.push_back('X');
-   int l = 0;
-   int name = 'A';
-
-   Node testNode = Node(name, l, vec);
-   cout << testNode.boolean_name;
-   cout << testNode.outgoing_connections.size() << endl;
-
-   vector<char> vec2; 
-   vec.push_back('W');
-   vec.push_back('Y');
-   l = 5;
-   name = 'X';
-   double prob = 0.13;
+   ifstream myfile; //Input file
+   myfile.open("input.txt");
+   vector<Node> BayNet; //Bayesian Network data structure
    
-   SourceNode secondTest = SourceNode(name, l, vec, prob);
-   cout << secondTest.boolean_name << secondTest.level << secondTest.prior_probability << endl;
+   int num_of_Snodes;
+   myfile >> num_of_Snodes; //Reads in # of source nodes
 
+   char bool_name;
+   int depth, num_of_edges;
+   double priorP;
+   for(int i = 0; i < num_of_Snodes; i++) //Reads in source nodes that are first in the file
+   {
+      myfile >> bool_name;
+      myfile >> priorP;
+      myfile >> depth;
+      myfile >> num_of_arcs;
 
+      vector<char> arcs; //Vector of edges new node will point to
+      char temp;
+      for(int j = 0; j < num_of_arcs; j++) //Adds the nodes that are being pointed to by new node
+      {
+         myfile >> temp;
+         arcs.push_back(temp);
+      }
+
+      SourceNode baynode = SourceNode(bool_name, depth, arcs, priorP); //Initialize source nodes
+      BayNet.push_back(baynode); //Add source node to network
+   }
+
+   int num_of_Cnodes;
+   myfile >> num_of_Cnodes;
+
+   for(int i = 0; i < num_of_Cnodes; i++)
+   {
+      
+   }
 }
