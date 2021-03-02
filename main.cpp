f3e9123faa20cbc132af66bf4e8c70a7b5699d9f
car5501 <cepeda.carlos2310@gmail.com>
2021-03-01 21:15:44 -0500

    Tried to convert from /r/n to /n
    It might not have worked

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 // AI Project
-
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -8,9 +7,7 @@
 #include <string>
 #include "node.h"
 #include "source_node.h"
-
 using namespace std;
-
 int main()
 {
    ifstream myfile; //Input file
@@ -29,14 +26,12 @@ int main()
       cout << "Error, too many source nodes!" << endl;
       exit(1);
    }
-
    for(int i = 0; i < num_nodes; i++) //Reads in source nodes that are first in the file
    {
       char node_name;
       myfile >> node_name;
       double prior_prob;
       myfile >> prior_prob;
-
       char num_of_arcs; //Reads and checks the number of arcs
       myfile >> num_of_arcs;
       total_arcs += num_of_arcs;
@@ -45,7 +40,6 @@ int main()
          cout << "Error, too many arcs" << endl;
          exit(1);
       }
-
       vector<char> arcs; //Vector of edges new node will point to
       char temp;
       for(int j = 0; j < num_of_arcs; j++) //Adds the nodes that are being pointed to by new node
@@ -53,11 +47,9 @@ int main()
          myfile >> temp;
          arcs.push_back(temp);
       }
-
       SourceNode baynode = SourceNode(node_name, level, arcs, prior_prob); //Initialize source nodes
       BayNet.push_back(baynode); //Add source node to network
    }
-
    //Conditional Nodes
    while(!myfile.eof()) //Will loop until all conditional nodes have been inputted
    {
@@ -68,7 +60,6 @@ int main()
          cout << "Error, too many levels to graph" << endl;
          exit(1);
       }
-
       myfile >> num_nodes; //Gets and checks number of condtional nodes on the current level
       total_nodes += num_nodes;
       if(total_nodes > 10)
@@ -76,18 +67,15 @@ int main()
          cout << "Error, too many source nodes!" << endl;
          exit(1);
       }
-
       for(int i = 0; i < num_nodes; i++) //Loops through all condtional nodes on the current level
       {
          char node_name;
          myfile >> node_name;
          int num_var_in_table;
          myfile >> num_var_in_table;
-
          vector<char> table_headers; //These 3 vectors will be the table that is stored in condtional nodes
          vector<string> bool_values;
          vector<double> prob_values;
-
          for(int j = 0; j < 2; j++) //Reads in all the table headers of current conditional node
          {
             char temp;
@@ -99,12 +87,10 @@ int main()
             string tempS;
             myfile >> tempS;
             bool_values.push_back(tempS);
-
             double tempD;
             myfile >> tempD;
             prob_values.push_back(tempD);
          }
-
          char num_of_arcs; //Gets and checks the number of arcs between nodes
          myfile >> num_of_arcs;
          total_arcs += num_of_arcs;
@@ -113,7 +99,6 @@ int main()
             cout << "Error, too many arcs" << endl;
             exit(1);
          }
-
          vector<char> arcs; //Vector of edges new node will point to
          char temp;
          for(int j = 0; j < num_of_arcs; j++) //Adds the nodes that are being pointed to by new node
@@ -121,11 +106,9 @@ int main()
             myfile >> temp;
             arcs.push_back(temp);
          }
-
          ConditionalNode baynode = ConditionalNode(node_name, level, arcs, num_var_in_table, table_headers, bool_values, prob_values); //Initializes conditional node
          BayNet.push_back(baynode); //Adds conditional node to network
       }
    }
-
    return 0;
 }
